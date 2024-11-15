5fdb87e18052476006282083df772214fd3aee49
Heyan Zhu <zhu_heyan@163.com>
2024-11-15 11:38:18 +0800

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -19,52 +19,58 @@ import EventDBManager from './libs/event-db-manager';
 import DeathEvent from "./utils/death-event";
 import DatabaseWrapper from "./utils/sqlite-wrapper";
 
-const pinoHttpConfig = {
-    autoLogging: false,
-    quietReqLogger: true,
-    quietResLogger: true
-};
-const pinoPrettyConfig: PinoPretty.PrettyOptions = {
-    colorize: true,
-    colorizeObjects: true,
-    translateTime: true,
+function initLogger() {
+    const pinoHttpConfig = {
+        autoLogging: false,
+        quietReqLogger: true,
+        quietResLogger: true
+    };
+    const pinoPrettyConfig: PinoPretty.PrettyOptions = {
+        colorize: true,
+        colorizeObjects: true,
+        translateTime: true,
+    }
+
+    const pinoPrettyInst = pinoPretty(pinoPrettyConfig);
+    const loggerHttp = pinoHttp(pinoHttpConfig, pinoPrettyInst)
+    const logger = pino(pinoPrettyInst);
+
+    return { logger, loggerHttp };
 }
 
-const configSchema = z.object({
-    DB_NAME: z.string(),
-    DB_DIR: z.string(),
+function initConfig() {
+    const configSchema = z.object({
+        DB_NAME: z.string(),
+        DB_DIR: z.string(),
 
-    SERVER_PORT: z.number(),
+        SERVER_PORT: z.number(),
 
-    IP_MAX_PER_MIN: z.number(),
-    BANNED_IP: z.array(z.string()),
-    TOKEN_SECRET_KEY: z.string(),
-    TOKEN_SALT_ROUND: z.number(),
-    TOKEN_EXPIRES_IN: z.string(),
-});
+        IP_MAX_PER_MIN: z.number(),
+        BANNED_IP: z.array(z.string()),
+        TOKEN_SECRET_KEY: z.string(),
+        TOKEN_SALT_ROUND: z.number(),
+        TOKEN_EXPIRES_IN: z.string(),
+    });
 
-const defaultConfigs: z.infer<typeof configSchema> = {
-    DB_NAME: "database",
-    DB_DIR: "./",
+    const defaultConfigs: z.infer<typeof configSchema> = {
+        DB_NAME: "database",
+        DB_DIR: "./",
 
-    SERVER_PORT: 3000,
-    IP_MAX_PER_MIN: 100,
-    BANNED_IP: [],
+        SERVER_PORT: 3000,
+        IP_MAX_PER_MIN: 100,
+        BANNED_IP: [],
 
-    TOKEN_SECRET_KEY: "secret",
-    TOKEN_SALT_ROUND: 12,
-    TOKEN_EXPIRES_IN: "1d",
+        TOKEN_SECRET_KEY: "secret",
+        TOKEN_SALT_ROUND: 12,
+        TOKEN_EXPIRES_IN: "1d",
+    }
+    return {
+        promise: new ConfigProvider(configSchema, defaultConfigs).getConfig(process.env.CONFIG_FILE || "config.yml"),
+        schema: configSchema
+    }
 }
 
-const pinoPrettyInst = pinoPretty(pinoPrettyConfig);
-const loggerHttp = pinoHttp(pinoHttpConfig, pinoPrettyInst)
-const logger = pino(pinoPrettyInst);
-
-const deathEvent = new DeathEvent(logger);
-deathEvent.addJob(() => {
-    logger.info("Shutting down server...");
-    return true;
-}, "Msg");
+type ConfigType = z.infer<Awaited<ReturnType<typeof initConfig>>["schema"]>;
 
 function attachRoutes(app: express.Application, sessionManager: SessionManger, studentDBManager: StudentDBManager, authenticationManager: AuthenticationManager, recruitmentDBManager: RecruitmentDBManager, eventsDBManager: EventDBManager) {
     serverRoutes.forEach(({ name, handler }) => {
@@ -87,7 +93,7 @@ function attachRoutes(app: express.Application, sessionManager: SessionManger, s
     });
 }
 
-function initManagers(db: DatabaseWrapper, config: z.infer<typeof configSchema>) {
+function initManagers(db: DatabaseWrapper, config: ConfigType) {
     return Promise.all([
         new Promise<StudentDBManager>((resolve) => {
             const studentDBManager = new StudentDBManager(db, () => resolve(studentDBManager));
@@ -105,53 +111,71 @@ function initManagers(db: DatabaseWrapper, config: z.infer<typeof configSchema>)
     ]);
 }
 
-function main(config: z.infer<typeof configSchema>) {
+function initExpress(config: ConfigType) {
+    const limiter = rateLimit({
+        windowMs: 60000,
+        limit: config.IP_MAX_PER_MIN,
+        message: 'Too many requests from this IP, please try again later.'
+    });
+
+    const configBannedIPs = config.BANNED_IP;
+    const bannedIPs = (Array.isArray(configBannedIPs) ? configBannedIPs : []) as string[];
+    const ipFilter = IpFilter(bannedIPs, { mode: 'deny' });
+
+    logger.info("Server is starting...");
+    const app = express()
+        .use(bodyParser.json())
+        .use(loggerHttp)
+        .use(limiter)
+        .use(ipFilter)
+        .use(cors());
+    return app;
+}
+
+function main(config: ConfigType) {
     return new Promise<void>((resolve, reject) => {
         try {
+            const deathEvent = new DeathEvent(logger);
+            deathEvent.addJob(() => {
+                logger.info("Shutting down server...");
+                return true;
+            }, "Msg");
+
             const db = new DatabaseWrapper(config.DB_NAME, config.DB_DIR, deathEvent, logger);
             const port = config.SERVER_PORT;
             const sessionManager = new SessionManger();
 
-            const limiter = rateLimit({
-                windowMs: 60000,
-                limit: config.IP_MAX_PER_MIN,
-                message: 'Too many requests from this IP, please try again later.'
-            });
-
-            const configBannedIPs = config.BANNED_IP;
-            const bannedIPs = (Array.isArray(configBannedIPs) ? configBannedIPs : []) as string[]
-            const ipFilter = IpFilter(bannedIPs, { mode: 'deny' })
-
-            logger.info("Server is starting...");
-            const app = express()
-                .use(bodyParser.json())
-                .use(loggerHttp)
-                .use(limiter)
-                .use(ipFilter)
-                .use(cors());
+            const app = initExpress(config);
 
             initManagers(db, config)
                 .then(([studentDBManager, authenticationManager, recruitmentDBManager]) => {
                     const eventsDBManager = new EventDBManager(db, studentDBManager, recruitmentDBManager);
                     attachRoutes(app, sessionManager, studentDBManager, authenticationManager, recruitmentDBManager, eventsDBManager);
+                    app.listen(port, '0.0.0.0', () => {
+                        logger.info(`Server is running at port ${port}`);
+                        resolve();
+                    });
                 });
-
-            app.listen(port, '0.0.0.0', () => {
-                logger.info(`Server is running at port ${port}`);
-                resolve();
-            });
         } catch (e) {
             reject(e);
         }
     });
 }
 
-new ConfigProvider(configSchema, defaultConfigs).getConfig("config.yml")
-    .then((config) => {
+const { logger, loggerHttp } = initLogger();
+const { promise: configPromise } = initConfig()
+
+configPromise
+    .then((config: ConfigType) => {
         logger.info("Config is ready");
-        main(config).then(() => {
-            logger.info("Server is ready");
-        })
+        main(config)
+            .then(() => {
+                logger.info("Server is ready");
+            })
+            .catch(e => {
+                logger.error("Fail to start server");
+                console.error(e);
+            });
     })
     .catch((err: z.ZodError | string) => {
         if (typeof err == "string") {
