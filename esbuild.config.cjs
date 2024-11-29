493593e0bb760b4fe84154d376bced4df6e873c4
Heyan Zhu <zhu_heyan@163.com>
2024-11-29 18:32:46 +0800

    improve build config compatibility

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -1,13 +1,7 @@
 const esbuild = require("esbuild");
-const { spawn } = require("child_process");
-const outFile = "dist/index.cjs";
-const ON_DEATH = require("death");
-const child_process = require("node:child_process");
 const fs = require('fs');
-const path = require('path');
 
 const outdir = "dist";
-
 const filesToCopy = [
 	'bcrypt-fast/bcrypt_fast_bg.wasm',
 	'node-sqlite3-wasm/dist/node-sqlite3-wasm.wasm'
@@ -29,36 +23,22 @@ const options = {
 	minify: process.argv[2] === "prod",
 };
 
-child_process.execSync(`rm -r ./dist/*`)
+fs.rmSync('./dist', { recursive: true, force: true });
 
+console.time("Build Time");
 esbuild
 	.build(options)
 	.then(() => {
+		console.timeEnd("Build Time");
 		filesToCopy.forEach(file => {
 			const from = `./node_modules/${file}`;
 			const to = `${outdir}/${file.split("/").pop()}`;
 			fs.copyFileSync(from, to);
 			console.log(`Copied ${from} to ${to}`);
 		});
-
-		if (process.argv[2] !== "dev") return;
-		const child = spawn("node " + outFile, {
-			shell: true,
-			stdio: ["pipe", "pipe", "pipe"],
-		});
-
-		child.stdout.pipe(process.stdout);
-		child.stderr.pipe(process.stderr);
-
-		ON_DEATH(() => {
-			child.kill();
-		});
-
-		child.on("exit", (code) => {
-			console.log(`\n- Child process exited with code ${code} -`);
-			process.exit(code);
-		});
 	})
 	.catch(() => {
+		console.error("Build failed");
+		console.timeEnd("Build Time");
 		process.exit(1);
 	});
